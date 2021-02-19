a3322dbbc8783bdd4eb789c74b4c5fec25ff3dca
vincentktieu@ucsb.edu <vincentktieu@ucsb.edu>
2021-02-18 20:40:20 -0800

    vt - finished TabId

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Header from "./components/Header";
 import Text from "./components/Text";
 import Images from "./components/Images";
 import Videos from "./components/Videos";
@@ -8,22 +8,63 @@ import Table from "./components/Table";
 import Email from "./components/Email";
 
 export default function App() {
+  let [TabId, setTabId] = useState(0);
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Body />
-    </BrowserRouter>
+    <>
+      <header>
+        <h3 className="nav-brand">Vincent Tieu's PA2</h3>
+        <nav>
+          <ul className="nav-links">
+            <li>
+              { 
+                TabId === 0 ? 
+                <a className="page-selected" onClick={() => setTabId(0)}>Text</a> : 
+                <a onClick={() => setTabId(0)}>Text</a>
+              }
+            </li>
+            <li>
+              {
+                TabId === 1 ?
+                <a className="page-selected" onClick={() => setTabId(1)}>Images</a> :
+                <a onClick={() => setTabId(1)}>Images</a>
+              }
+            </li>
+            <li>
+              {
+                TabId === 2 ?
+                <a className="page-selected" onClick={() => setTabId(2)}>Videos</a> :
+                <a onClick={() => setTabId(2)}>Videos</a>
+              }
+            </li>
+            <li>
+              {
+                TabId === 3 ?
+                <a className="page-selected" onClick={() => setTabId(3)}>Table</a> :
+                <a onClick={() => setTabId(3)}>Table</a>
+              }
+            </li>
+            <li>
+              {
+                TabId === 4 ?
+                <a className="page-selected" onClick={() => setTabId(4)}>Email</a> :
+                <a onClick={() => setTabId(4)}>Email</a>
+              }
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Body TabId={TabId}/>
+    </>
   )
 }
 
-function Body() {
+function Body(props) {
+  let TabId = props.TabId;
+  const components = [<Text/>, <Images/>, <Videos/>, <Table/>, <Email/>];
   return (
-    <Switch>
-      <Route path="/images" exact component={Images}></Route>
-      <Route path="/videos" exact component={Videos}></Route>
-      <Route path="/table" exact component={Table}></Route>
-      <Route path="/email" exact component={Email}></Route>
-      <Route path="/" component={Text} />
-    </Switch>
+    <>
+      {components[TabId]}
+    </>
   )
 }
